fb2911c49b211bb3acc4f53f29df9996ae1eea03
Thalles-HsA <thsa.henrique@gmail.com>
2023-02-07 15:58:20 -0300

    Finalizando o projeto.

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Botao from '../Botao';
 import Relogio from './Relogio';
 import style from './Cronometro.module.scss'
+import { ITarefa } from '../../types/tarefa';
+import { tempoParaSegundos } from '../../common/utils/time';
 
-// import { Container } from './styles';
+interface Props {
+  selecionado: ITarefa | undefined,
+  finalizaTarefa: () => void
+}
 
-const Cronometro: React.FC = () => {
+const Cronometro = ({selecionado, finalizaTarefa}: Props) => {
+
+  const [tempo, setTempo] = useState<number>(tempoParaSegundos(String(selecionado?.tempo)));
+
+  useEffect(() => {
+
+    if(selecionado?.tempo){
+      setTempo(tempoParaSegundos(selecionado?.tempo))
+    }
+  },[selecionado]);
+
+  function regressiva(contador: number = 0){
+    setTimeout(() => {
+      if(contador > 0) {
+        setTempo(contador - 1)
+        return regressiva(contador - 1)
+      }
+
+      finalizaTarefa();
+      
+    }, 1000)
+  }
+
+
   return (
     <div className={style.cronometro}>
         <p className={style.titluo}>Escolha um card e inicie o cronômetro</p>
         <div className={style.relogioWrapper}>
-          <Relogio/>
+          <Relogio tempo={tempo}/>
         </div>
-        <Botao>Começar</Botao>
+        <Botao onClick={()=>regressiva(tempo)} >
+          Começar
+        </Botao>
     </div>
     );
 }
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,12 +1,38 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 
-function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
+
+interface Props extends ITarefa {
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+
+function Item(
+    {
+        tarefa,
+        tempo,
+        selecionado,
+        completado,
+        id,
+        selecionaTarefa
+    }: Props) {
 
     return (
-        <li className={style.item}>
+        <li
+            className={`${style.item} ${selecionado ? style.itemSelecionado: ''} ${completado ? style.itemCompletado : ''}`}
+            onClick={() => !completado && selecionaTarefa(
+                {
+                    tarefa,
+                    tempo,
+                    completado,
+                    selecionado,
+                    id
+                }
+            )}
+        >
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado && <span className={style.concluido} aria-label='tarefa completada'></span>}
         </li>
     );
 }
